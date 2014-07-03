4ad5247e2d0ca6d7b2455dd1ba8d064d911b8e70
Robert Abraham <robert@adeven.com>
2014-07-03 18:04:52 +0200

    added operator tests

--- test/sql/operator.sql
+++ test/sql/operator.sql
@@ -8,10 +8,47 @@ SELECT '1=>1, -1=>3'::istore + '1=>1'::istore;
 SELECT '1=>1, -1=>3'::istore + '-1=>-1'::istore;
 SELECT '1=>1, -1=>3'::istore + '1=>-1'::istore;
 
+SELECT '1=>1, -1=>3'::istore + 1;
+SELECT '1=>1, -1=>3'::istore + 1;
+SELECT '1=>1, -1=>3'::istore + 1;
+
+SELECT '1=>1, -1=>3'::istore + 0;
+SELECT '1=>1, -1=>3'::istore + 0;
+SELECT '1=>1, -1=>3'::istore + 0;
+
+SELECT '1=>1, -1=>3'::istore + -1;
+SELECT '1=>1, -1=>3'::istore + -1;
+SELECT '1=>1, -1=>3'::istore + -1;
+
 SELECT '1=>1, -1=>3'::istore - '1=>1'::istore;
 SELECT '1=>1, -1=>3'::istore - '-1=>-1'::istore;
 SELECT '1=>1, -1=>3'::istore - '1=>-1'::istore;
 
+SELECT '1=>1, -1=>3'::istore - 1;
+SELECT '1=>1, -1=>3'::istore - 1;
+SELECT '1=>1, -1=>3'::istore - 1;
+
+SELECT '1=>1, -1=>3'::istore - 0;
+SELECT '1=>1, -1=>3'::istore - 0;
+SELECT '1=>1, -1=>3'::istore - 0;
+
+SELECT '1=>1, -1=>3'::istore - -1;
+SELECT '1=>1, -1=>3'::istore - -1;
+SELECT '1=>1, -1=>3'::istore - -1;
+
 SELECT '1=>3, 2=>2'::istore * '1=>2, 3=>5'::istore;
 SELECT '-1=>3, 2=>2'::istore * '-1=>2, 3=>5'::istore;
 SELECT '-1=>3, 2=>2'::istore * '-1=>-2, 3=>5'::istore;
+
+SELECT '1=>1, -1=>3'::istore * 1;
+SELECT '1=>1, -1=>3'::istore * 1;
+SELECT '1=>1, -1=>3'::istore * 1;
+
+SELECT '1=>1, -1=>3'::istore * 0;
+SELECT '1=>1, -1=>3'::istore * 0;
+SELECT '1=>1, -1=>3'::istore * 0;
+
+SELECT '1=>1, -1=>3'::istore * -1;
+SELECT '1=>1, -1=>3'::istore * -1;
+SELECT '1=>1, -1=>3'::istore * -1;
+
