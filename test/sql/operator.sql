9f7dfc1a5eacc931a6191442b29d2b97abca216c
Robert Abraham <robert@adeven.com>
2014-07-07 11:59:26 +0200

    fixed duplicate tests

--- test/sql/operator.sql
+++ test/sql/operator.sql
@@ -8,47 +8,40 @@ SELECT '1=>1, -1=>3'::istore + '1=>1'::istore;
 SELECT '1=>1, -1=>3'::istore + '-1=>-1'::istore;
 SELECT '1=>1, -1=>3'::istore + '1=>-1'::istore;
 
-SELECT '1=>1, -1=>3'::istore + 1;
-SELECT '1=>1, -1=>3'::istore + 1;
-SELECT '1=>1, -1=>3'::istore + 1;
-
-SELECT '1=>1, -1=>3'::istore + 0;
-SELECT '1=>1, -1=>3'::istore + 0;
-SELECT '1=>1, -1=>3'::istore + 0;
-
-SELECT '1=>1, -1=>3'::istore + -1;
-SELECT '1=>1, -1=>3'::istore + -1;
-SELECT '1=>1, -1=>3'::istore + -1;
+SELECT '1=>1, -1=>3'::istore  + 1;
+SELECT '-1=>1, 1=>3'::istore  + 1;
+SELECT '-1=>1, -1=>3'::istore + 1;
+SELECT '1=>1, -1=>3'::istore  + 0;
+SELECT '-1=>1, 1=>3'::istore  + 0;
+SELECT '-1=>1, -1=>3'::istore + 0;
+SELECT '1=>1, -1=>3'::istore  + -1;
+SELECT '-1=>1, 1=>3'::istore  + -1;
+SELECT '-1=>1, -1=>3'::istore + -1;
 
 SELECT '1=>1, -1=>3'::istore - '1=>1'::istore;
 SELECT '1=>1, -1=>3'::istore - '-1=>-1'::istore;
 SELECT '1=>1, -1=>3'::istore - '1=>-1'::istore;
 
-SELECT '1=>1, -1=>3'::istore - 1;
-SELECT '1=>1, -1=>3'::istore - 1;
-SELECT '1=>1, -1=>3'::istore - 1;
-
-SELECT '1=>1, -1=>3'::istore - 0;
-SELECT '1=>1, -1=>3'::istore - 0;
-SELECT '1=>1, -1=>3'::istore - 0;
-
-SELECT '1=>1, -1=>3'::istore - -1;
-SELECT '1=>1, -1=>3'::istore - -1;
-SELECT '1=>1, -1=>3'::istore - -1;
+SELECT '1=>1, -1=>3'::istore  - 1;
+SELECT '-1=>1, 1=>3'::istore  - 1;
+SELECT '-1=>1, -1=>3'::istore - 1;
+SELECT '1=>1, -1=>3'::istore  - 0;
+SELECT '-1=>1, 1=>3'::istore  - 0;
+SELECT '-1=>1, -1=>3'::istore - 0;
+SELECT '1=>1, -1=>3'::istore  - -1;
+SELECT '-1=>1, 1=>3'::istore  - -1;
+SELECT '-1=>1, -1=>3'::istore - -1;
 
 SELECT '1=>3, 2=>2'::istore * '1=>2, 3=>5'::istore;
 SELECT '-1=>3, 2=>2'::istore * '-1=>2, 3=>5'::istore;
 SELECT '-1=>3, 2=>2'::istore * '-1=>-2, 3=>5'::istore;
 
-SELECT '1=>1, -1=>3'::istore * 1;
-SELECT '1=>1, -1=>3'::istore * 1;
-SELECT '1=>1, -1=>3'::istore * 1;
-
-SELECT '1=>1, -1=>3'::istore * 0;
-SELECT '1=>1, -1=>3'::istore * 0;
-SELECT '1=>1, -1=>3'::istore * 0;
-
-SELECT '1=>1, -1=>3'::istore * -1;
-SELECT '1=>1, -1=>3'::istore * -1;
-SELECT '1=>1, -1=>3'::istore * -1;
-
+SELECT '1=>1, -1=>3'::istore  * 1;
+SELECT '-1=>1, 1=>3'::istore  * 1;
+SELECT '-1=>1, -1=>3'::istore * 1;
+SELECT '1=>1, -1=>3'::istore  * 0;
+SELECT '-1=>1, 1=>3'::istore  * 0;
+SELECT '-1=>1, -1=>3'::istore * 0;
+SELECT '1=>1, -1=>3'::istore  * -1;
+SELECT '-1=>1, 1=>3'::istore  * -1;
+SELECT '-1=>1, -1=>3'::istore * -1;
