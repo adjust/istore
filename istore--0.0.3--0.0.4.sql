9f209df890e34c8db18daecdefa1406cfa9260f0
Manuel Kniep <m.kniep@web.de>
2014-11-27 17:40:58 +0100

    migrations

--- istore--0.0.3--0.0.4.sql
+++ istore--0.0.3--0.0.4.sql
@@ -3,16 +3,16 @@ CREATE OR REPLACE FUNCTION istore_array_add(country[], integer[])
  RETURNS country_istore
  LANGUAGE c
  IMMUTABLE STRICT
-AS '$libdir/istore.so', $function$istore_array_add$function$;
+AS '$libdir/istore.so', $function$country_istore_array_add$function$;
 ----
 CREATE OR REPLACE FUNCTION istore_array_add(device_type[], integer[])
  RETURNS device_istore
  LANGUAGE c
  IMMUTABLE STRICT
-AS '$libdir/istore.so', $function$istore_array_add$function$;
+AS '$libdir/istore.so', $function$device_type_istore_array_add$function$;
 ----
 CREATE OR REPLACE FUNCTION istore_array_add(os_name[], integer[])
  RETURNS os_name_istore
  LANGUAGE c
  IMMUTABLE STRICT
-AS '$libdir/istore.so', $function$istore_array_add$function$;
+AS '$libdir/istore.so', $function$os_name_istore_array_add$function$;
