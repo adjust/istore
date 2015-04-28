f7eae1b247e2fa4bb05029ad94a873b247a7b29e
Manuel Kniep <m.kniep@web.de>
2015-04-28 19:41:47 +0200

    fix migration

--- istore--0.0.4--0.0.5.sql
+++ istore--0.0.4--0.0.5.sql
@@ -1,4 +1,10 @@
 ----functions----
+CREATE OR REPLACE FUNCTION accumulate(istore, integer)
+ RETURNS istore
+ LANGUAGE c
+ IMMUTABLE STRICT
+AS '$libdir/istore.so', $function$istore_accumulate$function$;
+----
 CREATE OR REPLACE FUNCTION accumulate(istore)
  RETURNS istore
  LANGUAGE c
