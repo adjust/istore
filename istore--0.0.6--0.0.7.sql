3e9015d336c3e7d8b1508a0195d3d94a74f5fc1e
Manuel Kniep <m.kniep@web.de>
2015-08-18 11:35:38 +0200

    remove compatibility with other versions as the binary representation changes version 0.1.x is no longer compatible with 0.0.x versions

--- istore--0.0.6--0.0.7.sql
+++
@@ -1,6 +0,0 @@
-----functions----
-CREATE OR REPLACE FUNCTION istore_seed(integer, integer, bigint)
- RETURNS istore
- LANGUAGE c
- IMMUTABLE
-AS '$libdir/istore.so', $function$istore_seed$function$;
--- istore--0.0.8--0.0.7.sql
+++
@@ -1,6 +0,0 @@
-----functions----
-DROP FUNCTION gin_consistent_istore_key(internal, smallint, internal, integer, internal, internal);
-----
-DROP FUNCTION gin_extract_istore_key_query(internal, internal, smallint, internal, internal);
-----
-DROP FUNCTION gin_extract_istore_key(internal, internal);
