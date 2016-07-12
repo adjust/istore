559b4593e42c55d34bbdf6ad508a970a7c3c3057
nikola <nikola@howkul.info>
2016-07-12 16:16:24 +0200

    Fix allocation size

--- src/istore_io.c
+++ src/istore_io.c
@@ -289,7 +289,7 @@ bigistore_to_json(PG_FUNCTION_ARGS)
     if (is->len == 0)
         PG_RETURN_TEXT_P(cstring_to_text_with_len("{}", 2));
 
-    buf = palloc(sizeof(char) * 25+1);
+    buf = palloc(sizeof(char) * (25+1));
     pairs   = FIRST_PAIR(is, BigIStorePair);
     initStringInfo(&dst);
     appendStringInfoChar(&dst, '{');
