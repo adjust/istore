ec99c2430398315fd1f75a8daa893d81b74e7823
Artur Zakirov <zaartur@gmail.com>
2023-04-06 12:09:52 +0200

    Get rid of Ruby tests generation scripts.
    These Ruby scripts creates unnecessary complexity and it is easier
    to write tests manually.

--- test/sql/functions_test.sql
+++ test/sql/functions_test.sql
@@ -1,6 +1,3 @@
-BEGIN;
-CREATE EXTENSION istore;
-
 -- istore test cases
 SELECT max_value(''::istore);
 SELECT max_value('1=>1'::istore);
@@ -15,5 +12,3 @@ SELECT max_value('1=>1, 2=>1'::bigistore);
 SELECT max_value('0=>2, 1=>4, 3=>0 ,2=>2'::bigistore);
 SELECT max_value('10=>21, 11=>500, 13=>800 ,12=>4200'::bigistore);
 SELECT max_value('10=>21, 11=>500, 13=>800 ,12=>3147483647'::bigistore);
-
-ROLLBACK;
--- test/sql/istore_key_gin_test.sql
+++ test/sql/istore_key_gin_test.sql
@@ -1,128 +1,88 @@
-BEGIN;
 -- bigistore gin access should do index scan using gin;
--- ./spec/istore/istore_key_gin_spec.rb:23;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t1 AS
 SELECT i, string_agg(j::text || '=>0', ',')::bigistore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t1 USING gin(s);;
 SET enable_seqscan = 0;;
-EXPLAIN (COSTS OFF) SELECT * FROM t WHERE s ? 3;;
+EXPLAIN (COSTS OFF) SELECT * FROM t1 WHERE s ? 3;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- bigistore gin access should find the matching rows for key 300;
--- ./spec/istore/istore_key_gin_spec.rb:31;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t2 AS
 SELECT i, string_agg(j::text || '=>0', ',')::bigistore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t2 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 300 ORDER BY i;;
+SELECT s FROM t2 WHERE s ? 300 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- bigistore gin access should find the matching rows for key 600;
--- ./spec/istore/istore_key_gin_spec.rb:40;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t3 AS
 SELECT i, string_agg(j::text || '=>0', ',')::bigistore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t3 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 600 ORDER BY i;;
+SELECT s FROM t3 WHERE s ? 600 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- bigistore gin access should find the matching rows for key 900;
--- ./spec/istore/istore_key_gin_spec.rb:49;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t4 AS
 SELECT i, string_agg(j::text || '=>0', ',')::bigistore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t4 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 900 ORDER BY i;;
+SELECT s FROM t4 WHERE s ? 900 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- istore gin access should do index scan using gin;
--- ./spec/istore/istore_key_gin_spec.rb:23;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t5 AS
 SELECT i, string_agg(j::text || '=>0', ',')::istore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t5 USING gin(s);;
 SET enable_seqscan = 0;;
-EXPLAIN (COSTS OFF) SELECT * FROM t WHERE s ? 3;;
+EXPLAIN (COSTS OFF) SELECT * FROM t5 WHERE s ? 3;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- istore gin access should find the matching rows for key 300;
--- ./spec/istore/istore_key_gin_spec.rb:31;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t6 AS
 SELECT i, string_agg(j::text || '=>0', ',')::istore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t6 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 300 ORDER BY i;;
+SELECT s FROM t6 WHERE s ? 300 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- istore gin access should find the matching rows for key 600;
--- ./spec/istore/istore_key_gin_spec.rb:40;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t7 AS
 SELECT i, string_agg(j::text || '=>0', ',')::istore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t7 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 600 ORDER BY i;;
+SELECT s FROM t7 WHERE s ? 600 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
-BEGIN;
 -- istore gin access should find the matching rows for key 900;
--- ./spec/istore/istore_key_gin_spec.rb:49;
-CREATE EXTENSION istore;
-CREATE TABLE t AS
+CREATE TABLE t8 AS
 SELECT i, string_agg(j::text || '=>0', ',')::istore AS s
 FROM
-generate_series(0, 100000) AS i,
+generate_series(0, 10000) AS i,
 generate_series(i, i + (i % 10)) AS j
 GROUP BY i;
-;
-CREATE INDEX ON t USING gin(s);;
+CREATE INDEX ON t8 USING gin(s);;
 SET enable_seqscan = 0;;
-SELECT s FROM t WHERE s ? 900 ORDER BY i;;
+SELECT s FROM t8 WHERE s ? 900 ORDER BY i;;
 RESET enable_seqscan;;
-ROLLBACK;
